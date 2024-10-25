5c1e1be3ba3a1dea74991fb09ca3502cff302458
LucasBP23 <lucasbraga2304@gmail.com>
2024-10-24 22:15:08 -0300

    Formulário professor e API IBGE
    Alteração parcial no formulário do professor e adição da API do IBGE para cidades e estados.

---
+++ Pacote_Download/Projeto_Seminário/Page_secretaria/cad_professor.js
@@ -0,0 +1,49 @@
+// Função para carregar os estados
+function carregarEstados() {
+    fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome')
+        .then(response => response.json())
+        .then(estados => {
+            const estadoSelect = document.getElementById('professor_estado');
+            estados.forEach(estado => {
+                let option = document.createElement('option');
+                option.value = estado.sigla;
+                option.text = estado.sigla;
+                estadoSelect.add(option);
+            });
+        })
+        .catch(error => console.error('Erro ao carregar os estados:', error));
+}
+
+// Função para carregar as cidades quando um estado for selecionado
+function carregarCidades(estadoId) {
+    const cidadeSelect = document.getElementById('professor_cidade'); // 
+    cidadeSelect.disabled = true; // Desabilitar o campo enquanto carrega as cidades
+    cidadeSelect.innerHTML = '<option value="">Selecione uma cidade</option>'; // Resetar as opções
+
+    fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${estadoId}/municipios?orderBy=nome`)
+        .then(response => response.json())
+        .then(cidades => {
+            cidades.forEach(cidade => {
+                let option = document.createElement('option');
+                option.value = cidade.id;
+                option.text = cidade.nome;
+                cidadeSelect.add(option);
+            });
+            cidadeSelect.disabled = false; // Reabilitar o campo após carregar as cidades
+        })
+        .catch(error => console.error('Erro ao carregar as cidades:', error));
+}
+
+// Adicionar eventos
+document.getElementById('professor_estado').addEventListener('change', function() { // Ajustado para professor_estado
+    const estadoId = this.value;
+    if (estadoId) {
+        carregarCidades(estadoId);
+    } else {
+        document.getElementById('professor_cidade').disabled = true; // Ajustado para professor_cidade
+        document.getElementById('professor_cidade').innerHTML = '<option value="">Selecione uma cidade</option>';
+    }
+});
+
+// Chamar a função de carregar estados ao carregar a página
+carregarEstados();
--- Pacote_Download/Projeto_Seminário/Page_secretaria/cad_professor.php
+++ Pacote_Download/Projeto_Seminário/Page_secretaria/cad_professor.php
@@ -1,3 +1,6 @@
+
+
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -6,7 +9,7 @@
     <title>Document</title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <link rel="icon" type="image/x-icon" href="Images/UniSGE-removebg-preview.png"> <!--Coloca o logo no topo da guia da página-->
-    <link rel="stylesheet" href="cad_professor.css">
+    <link rel="stylesheet" href="cad_professor.css?v=1.3">
    
       
         
@@ -23,117 +26,60 @@
                     <br>
                     <div class="inputBox">
                         
-                        <input type="text" name="nome" id="nome" class="inputUser" required>
-                        <label for="nome" class="labelInput">Nome completo: </label>
+                        <input type="text" name="professor_nome_completo" id="professor_nome_completo" class="inputUser" required>
+                        <label for="professor_nome_completo" class="labelInput">Nome completo: </label>
 
+                    </div>
+                    <br><br>
+                    <div class="inputBox">
+                    <label for="professor_data_nascimento"><b>Data de Nascimento:</b></label>
+                    <input type="date" name="professor_data_nascimento" id="professor_data_nascimento" class="inputUser" required>
                     </div>
                     <br><br>
 
                     <div class="inputBox">
-                        <input type="number" name="cpf" id="cpf" class="inputUser" required>
-                        <label for="cpf" class="labelInput">CPF: </label>
+                        <input type="text" name="professor_cpf" id="professor_cpf" class="inputUser" required>
+                        <label for="professor_cpf" class="labelInput">CPF: </label>
                     </div>
                     <br><br>
 
+                 
+                    <section class="coluna-50l">
                     <div class="inputBox">
-                        <input type="text" name="email" id="email" class="inputUser"  required>
-                        <label for="email" class="labelInput">Email: </label>
+                    <label for="professor_estado" > <b>Estado: </b> </label>
+                    <br>
+                    <select id="professor_estado" name="professor_estado" class="inputUser" required>
+                    <option value="">Selecione um estado</option>
+                    </select>
+                    </div>
+                    </section>
+                    
+                    <section class="coluna-50r">
+                    <div class="inputBox">
+                    <label for="professor_cidade" > <b>Cidade: </b> </label>
+                    <select id="professor_cidade" name="professor_cidade" class="inputUser" disabled required>
+                    <option value="">Selecione uma cidade</option>
+                    </select>
+                    </div>
+                    </section>
+
+                    <br><br><br>
+
+                    <div class="inputBox">
+                        <input type="text" name="professor_endereco" id="professor_endereco" class="inputUser"  required>
+                        <label for="professor_endereco" class="labelInput">Endereço: </label>
                       
                         
                     </div>
+
                     <br><br>
-                    <div class="inputBox">
-                        <input type="tel" name="telefone" id="telefone" class="inputUser"  required>
-                        <label for="telefone" class="labelInput">Telefone: </label>
-                        
-                    
-                    </div>
-                    <p>Sexo:</p>
-                    <input type="radio" id="feminino" name="sexo" value="feminino" required>
-                    <label for="feminino">Feminino</label>
-                    <br>
-                    <input type="radio" id="masculino" name="sexo" value="masculino" required>
-                    <label for="masculino">Masculino</label>
-                    <br>
-                    <input type="radio" id="outro" name="sexo" value="outro" required>
-                    <label for="outro">Outro</label>
-                    <br><br>
-    
-                    <label for="nascimento"><b>Data de Nascimento:</b></label>
-                    <input type="date" name="nascimento" id="nascimento" class="caixa_form" required>
-                    <br><br><br>
-    
-                    <div class="inputBox">
-                        <input type="text" name="materia" id="materia" class="inputUser"   required>
-                        <label for="materia" class="labelInput">Matéria</label>
-                       
-                        
-                    </div>
-                    <br>
-                    <H4>Titulação máxima: 
-                    <select required name="titulacao_max" id="titulacao_max" class="caixa_form">
-                        <option selected disabled value="">Escolha uma titulação</option>
-                        <option value="licenciatura">Licenciatura</option>
-                        <option value="bacharelado">Bacharelado</option>
-                        <option value="mestrado">Mestrado</option>
-                        <option value="doutorado">Doutorado</option>
-                        <option value="outro">Outro</option>
-                    </select>
-                </H4>
-                    <br>
-
-                    <section class="coluna-50l">
-                        <div class="inputBox">
-                            <input type="text" name="cidade" id="cidade" class="inputUser"  required>
-                            <label for="cidade" class="labelInput">Cidade: </label>
-                        </div>
-                        </section>
-
-
-                        <section class="coluna-50r">
-                    <div class="inputBox">
-                        <input type="" name="estado" id="estado" class="inputUser"  required>
-                        <label for="estado" class="labelInput">Estado</label>
-                    </div>
-                    </section>    
-
-                   
-                    <br><br><br>
-    
-                    <div class="inputBox">
-                        <input type="text" name="logradouro" id="logradouro" class="inputUser"  required>
-                        <label for="logradouro" class="labelInput">Logradouro: </label>
-                        
-                    </div>
-                    <br><br>
-
-                    <section class="coluna-50l">
-                    <div class="inputBox">
-                        <input type="number" name="numero" id="numero" class="inputUser"  required>
-                        <label for="numero" class="labelInput">Número: </label>
-                    </div>
-                    </section>
-                   
-                    <section class="coluna-50r">
-                    <div class="inputBox">
-                        <input type="text" name="bairro" id="bairro" class="inputUser"  required>
-                        <label for="bairro " class="labelInput">Bairro: </label>
-                    </div>
-                    </section>
-                    
-                    <br><br><br>
+                
+        
 
                     <div class="inputBox">
-                        <input type="text" name="complemento" id="complemento" class="inputUser" required>
-                        <label for="complemento" class="labelInput">Complemento: </label>
-                        
-                    </div>
-                   
 
-                    <h4>Cadastre uma senha</h4>
-                    <div class="inputBox">
-                        <input type="password" name="professor_senha" id="professor_senha" class="inputUser" required>
-                        <label for="professor_senha"class="labelInput">Senha: </label>
+                        <input type="password" name="professor_senha_acesso" id="professor_senha_acesso" class="inputUser" required>
+                        <label for="professor_senha_acesso"class="labelInput">Senha: </label>
                     </div>
                     <br><br>
 
@@ -150,6 +96,6 @@
 
 
 
-
+    <script src="cad_professor.js?v=1.1"></script>
 </body>
 </html>

ef085e7793d173f6f71d49aa9e3b45c48a066e8a
LucasBP23 <lucasbraga2304@gmail.com>
2024-11-04 21:21:04 -0300

    Exclusão de arquivo teste

--- Pacote_Download/Projeto_Seminário/rota_gerenciamento/listar_professor.php
+++
@@ -1,8 +0,0 @@
-function listarProfessores() {
-    $pdo = getConnection();
-    
-    $stmt = $pdo->query("SELECT * FROM professor");
-    $professores = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    
-    echo json_encode($professores);
-}
